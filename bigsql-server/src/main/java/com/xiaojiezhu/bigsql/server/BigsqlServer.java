03967c7e5a61097e610309cb36571addaf6e3dd8
朱小杰 <zxjother@163.com>
2018-08-17 14:34:27 +0800

    修改日志文件代码

--- bigsql-server/src/main/java/com/xiaojiezhu/bigsql/server/BigsqlServer.java
+++ bigsql-server/src/main/java/com/xiaojiezhu/bigsql/server/BigsqlServer.java
@@ -7,9 +7,12 @@ import com.xiaojiezhu.bigsql.server.config.ConfigLoader;
 import com.xiaojiezhu.bigsql.server.config.FileConfigLoader;
 import com.xiaojiezhu.bigsql.util.Asserts;
 import com.xiaojiezhu.bigsql.util.EnvUtil;
+import org.apache.logging.log4j.core.config.ConfigurationSource;
+import org.apache.logging.log4j.core.config.Configurator;
 
 import java.io.File;
-import java.net.URI;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * main class
@@ -25,16 +28,22 @@ public class BigsqlServer {
         Asserts.notNull(bigSqlConfig, "bigsql config can not be null");
 
         initLog4j2();
+
         Bootstrap bootstrap = new DefaultBootstrap();
         bootstrap.startServer(bigSqlConfig);
+
+        initLog4j2();
+
 
     }
 
     private static void initLog4j2() {
-        File f = new File(EnvUtil.getBigsqlConfPath() + "log4j2.xml");
-        URI fc = f.toURI();
-        org.apache.logging.log4j.core.Logger l = (org.apache.logging.log4j.core.Logger) org.apache.logging.log4j.LogManager
-                .getLogger(org.apache.logging.log4j.LogManager.ROOT_LOGGER_NAME);
-        l.getContext().setConfigLocation(fc);
+        File log4jFile = new File(EnvUtil.getBigsqlConfPath() + "log4j2.xml");
+        try {
+            ConfigurationSource source = new ConfigurationSource(new FileInputStream(log4jFile), log4jFile);
+            Configurator.initialize(null, source);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
